58100e79a604efe72c4eaf3b2e82bf65afc1f983
BhavyashKoluguri <102355627+BhavyashKoluguri@users.noreply.github.com>
2024-01-26 18:15:06 +0530

    Update bubble sort.java
    made the code optimised

--- bubble sort.java
+++ bubble sort.java
@@ -10,9 +10,7 @@ class HelloWorld {
             flag = false;
             for(int j=1; j<n-i; j++){
                 if(arr[j-1] > arr[j]){
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
+                    swap(arr,j-1,j);
                     flag = true;
                 }
             }
@@ -21,5 +19,10 @@ class HelloWorld {
             }
         }
         System.out.println(Arrays.toString(arr));
+    }
+    public static void swap(int[]nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
